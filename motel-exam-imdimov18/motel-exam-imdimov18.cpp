d49fefe8783489fb3d9ba7ca719097e225f296d1
IMDimov18 <IMDimov18@codingburgas.bg>
2021-06-22 09:12:33 +0300

    initial commit

--- motel-exam-imdimov18/motel-exam-imdimov18.cpp
+++ motel-exam-imdimov18/motel-exam-imdimov18.cpp
@@ -1,20 +1,62 @@
-// motel-exam-imdimov18.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct ROOM_PRICING
+{
+    float sRoomPrice;
+    float dRoomPrice;
+    float maisonettePrice;
+};
+
+struct BOOKING
+{
+    string clientNames;
+    string roomTypes;
+    int nights;
+    int rentNumber;
+};
+
+class Motel
+{
+public:
+
+    Motel(string name,int capacity)
+    {
+
+    }
+
+    ROOM_PRICING getRoomPricing()
+    {
+        ROOM_PRICING price;
+        return price;
+    }
+
+    string rentRoom(string clientName, string roomType, int numberOfNights)
+    {
+        
+    }
+
+    int checkOut(int bookingNumber)
+    {
+
+    }
+
+private:
+    string name;
+    int capacity;
+    vector<int> bookings;
+    int currentBookingNumber = 1;
+    int sRoomCount;
+    int dRoomCount;
+    int maisonetteCount;
+    int d = 40;
+    int s = 40;
+    int m = 20;
+};
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
